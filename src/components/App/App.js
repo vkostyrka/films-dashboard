2908795c9e0e19bff2833b88c0d52cb9719724cb
Vitalii Kostyrka <vkosturka@solanolabs.com>
2019-07-30 14:34:11 +0300

    Make Search by name request

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import HeaderTabs from "../HeaderTabs/HeaderTabs";
 
 function App() {
   return (
     <div className="App">
       Main Page
+      <HeaderTabs />
     </div>
   );
 }
